79caf8379c3567673b6fb3601bfec9863fcebf13
John Daugherty <johndaugherty1986@gmail.com>
2018-06-06 23:34:16 -0500

    creating more components

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SearchBar from '../SearchBar';
+import SearchResults from '../SearchResults';
+import Playlist from '../Playlist';
+import './App.css';
+
+
+  render() {
+    return (
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <!-- Add a SearchBar component -->
+        <div className="App-playlist">
+          <!-- Add a SearchResults component -->
+          <!-- Add a Playlist component -->
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
