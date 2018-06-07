d123c7ad3e9e92daac7d95f4bd699a6875a65834
John Daugherty <johndaugherty1986@gmail.com>
2018-06-07 00:13:06 -0500

    updated components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import SearchBar from '../SearchBar';
-import SearchResults from '../SearchResults';
-import Playlist from '../Playlist';
+import {SearchBar} from '../SearchBar';
+import {SearchResults} from '../SearchResults';
+import {Playlist} from '../Playlist';
 import './App.css';
 
 
@@ -10,7 +10,7 @@ import './App.css';
       <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <!-- Add a SearchBar component -->
+        <SearchBar />
         <div className="App-playlist">
           <!-- Add a SearchResults component -->
           <!-- Add a Playlist component -->
---
+++ src/Components/Playlist/PlayList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PlayList.css';
+import { TrackList } from '../TrackList/TrackList';
+
+class PlayList extends React.Component{
+  render(){
+    return(
+      <div class="Playlist">
+        <input value="New Playlist"/>
+        <TrackList />
+        <a class="Playlist-save">SAVE TO SPOTIFY</a>
+      </div>
+    )
+  }
+}
+export default PlayList;
